c63265a506de5fd8f778d9c707a21ca357b8e28a
comtom <tomas@comtom.com.ar>
2014-10-05 14:21:44 -0300

    misc, si no hay posts cargo mensaje de ayuda

--- php/templates/cargarmas.php
+++ php/templates/cargarmas.php
@@ -1,12 +1,14 @@
-<!-- cargar mas -->
-<div class="ui full page column grid">
-  <div class="ui column" style="margin-bottom:0px;">
-    <a href="#">
-    <div class="ui segment">
-      <center>
-        Cargar m&aacute;s publicaciones <i class="ui icon arrow down"></i>
-      </center>
+<?php if (!empty($posts)) { ?>
+    <!-- cargar mas -->
+    <div class="ui full page column grid">
+      <div class="ui column" style="margin-bottom:0px;">
+        <a href="#">
+        <div class="ui segment">
+          <center>
+            Cargar m&aacute;s publicaciones <i class="ui icon arrow down"></i>
+          </center>
+        </div>
+       </a>
+      </div>
     </div>
-   </a>
-  </div>
-</div>
+<?php } ?>
--- php/templates/novedades.php
+++ php/templates/novedades.php
@@ -1,25 +1,40 @@
 <?php global $posts ?>
-<?php foreach ($posts as $post) { ?>
-          <!-- post -->
-          <div class="ui full page column grid">
-            <div class="ui column" style="margin-bottom:0px;">
-              <div class="ui stacked segment">
+<?php if (empty($posts)) { ?>
+  <!-- no hay posts -->
+  <div class="ui full page column grid">
+    <div class="ui column" style="margin-bottom:0px;">
+      <div class="ui stacked segment">
+        <h2>No tienes publicaciones</h2>
+        <p>
+          Puedes <a href="/publicar">escribir publicaciones</a> en tu muro, o bien <a href="/amigos">agregar amigos</a> y escribir en sus muros.<br>
+          Ten en cuenta que para escribir en sus muros, deben confirmar tu solicitud de amistad
+        </p>
+      </div>
+    </div>
+  </div>
+<?php } else { ?>
+  <?php foreach ($posts as $post) { ?>
+    <!-- post -->
+    <div class="ui full page column grid">
+      <div class="ui column" style="margin-bottom:0px;">
+        <div class="ui stacked segment">
 
-                <div class="ui two column grid">
-                  <div class="ui column" style="max-width:108px;">
-                    <div class="ui circular segment" style="padding:5px;">
-                      <img class="ui circular image" src="perfil/<?php echo $post->usuario_id ?>.jpg" alt="" style="width:64px; height:64px;" />
-                    </div>
-                  </div>
-                  <div class="ui column" style="margin:0px;">
-                    <div>
-                      <p><big><strong><?php echo $post->nombre ?> <?php echo $post->apellido ?> (<?php echo $post->usuario ?>)</strong></big>
-                      <br><small><?php echo $post->fecha ?></small></p>
-                    </div>
-                  </div>
-                </div>
-                <p><?php echo utf8_encode($post->texto) ?></p>
+          <div class="ui two column grid">
+            <div class="ui column" style="max-width:108px;">
+              <div class="ui circular segment" style="padding:5px;">
+                <img class="ui circular image" src="perfil/<?php echo $post->usuario_id ?>.jpg" alt="" style="width:64px; height:64px;" />
+              </div>
+            </div>
+            <div class="ui column" style="margin:0px;">
+              <div>
+                <p><big><strong><?php echo $post->nombre ?> <?php echo $post->apellido ?> (<?php echo $post->usuario ?>)</strong></big>
+                <br><small><?php echo $post->fecha ?></small></p>
               </div>
             </div>
           </div>
+          <p><?php echo utf8_encode($post->texto) ?></p>
+        </div>
+      </div>
+    </div>
+  <?php } ?>
 <?php } ?>
